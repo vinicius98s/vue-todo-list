1e854f36f712b504b4171fce532e521ebe1e2b2d
Vinicius Sales <vinicius.2010.s@gmail.com>
2019-09-02 07:57:59 -0300

    Finish todo list

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Todos,
+    Header,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    };
+  },
+  methods: {
+    async deleteTodo(id) {
+      try {
+        this.todos = this.todos.filter(todo => todo.id !== id);
+        await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    async addTodo(todo) {
+      const { title, completed } = todo;
+
+      try {
+        const { data: todo } = await axios.post(
+          "https://jsonplaceholder.typicode.com/todos",
+          {
+            title,
+            completed
+          }
+        );
+
+        this.todos = [...this.todos, todo];
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  },
+  created: async function() {
+    const { data: todos } = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos?_limit=10"
+    );
+
+    this.todos = todos;
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<style lang="scss">
+* {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+
+  &:hover {
+    background: #666;
+  }
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete': todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{ todo.title }}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+
+  p {
+    input {
+      margin-right: 5px;
+    }
+  }
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: red;
+  color: white;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+  font-weight: bold;
+}
+</style>
